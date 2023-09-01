68d734246a1f7f75ce3ef48bb6656601cf30f85a
Febrianrifki <febrianrifki1702@gmail.com>
2023-09-01 18:04:45 +0700

    change TTL duration

--- app/models/user_session.js
+++ app/models/user_session.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const userSessionSchema = new mongoose.Schema({
     token: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }, // Tambahkan kolom createdAt
+    createdAt: { type: Date, default: Date.now },
 });
-userSessionSchema.index({ createdAt: 1 }, { expireAfterSeconds: 360 });
+
+//create TTL to delete token value if expired
+userSessionSchema.index({ createdAt: 1 }, { expireAfterSeconds: 3600 });
 const UserSession = mongoose.model('UserSession', userSessionSchema);
 
 module.exports = UserSession;
--- app/routes/user_route.js
+++ app/routes/user_route.js
@@ -1,7 +1,6 @@
 const userRoute = require('express').Router();
 const { user } = require('../controllers');
 
-
 userRoute.post('/user/register', user.register);
 userRoute.post('/user/login', user.login);
 userRoute.get('/user/logout', user.logout);
