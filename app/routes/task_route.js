c2814ab82b02b3683dbca7cb8012f5f1997b5857
Febrianrifki <febrianrifki1702@gmail.com>
2023-09-02 06:22:06 +0700

    add page and limit to fetch all data swagger

--- app/routes/task_route.js
+++ app/routes/task_route.js
@@ -61,6 +61,17 @@ taskRoute.delete('/task/:id', task.deleteTask);
  *     summary: Fetch all task
  *     tags:
  *        - Tasks
+ *     parameters:
+ *        - in: query
+ *          name: page
+ *          schema:
+ *            type: integer
+ *          description: Page number
+ *        - in: query
+ *          name: limit
+ *          schema:
+ *            type: integer
+ *          description: Number of items per page     
  *     responses:
  *       '200':
  *            description: Success
